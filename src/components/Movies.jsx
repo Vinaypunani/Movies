9b35101810b8c799c27998c49c0f7c4827407d56
Vinaypunani <114213456+Vinaypunani@users.noreply.github.com>
2024-12-09 13:02:59 +0530

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,7 +8,7 @@ const Movies = () => {
   
   if (!loading) {
     return (
-      <div className="flex justify-center items-center h-1/2">
+      <div className="flex justify-center items-start h-screen">
         <div className="text-white text-2xl">Loading...</div>
       </div>
     )
